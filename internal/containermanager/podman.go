2f79903055d7c962cef23fabb6102ccac3f76fe4
Lewis Denny <lewisdenny@me.com>
2024-12-19 00:46:14 +1000

    Lay initial foundation and Podman containermanager implementation
    This patch adds the first main component to the Pulpod project, the
    containermanager interface which will be used by the project to spawn
    container based plugins.
    
    As this is the first main component added to the project some extras are
    also being bundled in the simplest implementation possible, these
    include:
    - Basic logging using `go.uber.org/zap`
    - Podman in Podman testing
    - Config management using `github.com/knadh/koanf/v2`
    
    The meat of the is patch is the containermanager interface, specifically
    the PodmanManager implementation. This is a wrapper around
    `github.com/containers/podman/v5/pkg/bindings` to allow us to spawn
    container based plugins, Pulpod itself will be running in a container
    with the host Podman socket being mounted.
    
    Testing is implemented in a way to best mirror the real topology
    
    Ubuntu provided VM from GitHub
    ------------------------------
    Fedora Podman container "podman_host"
    ------------------------------
    Fedora container "pulpod_test"
    
    This means the code being tested runs inside pulpod_test running on
    Fedora with access to a Podman socket and can spawn containers on
    "pulpod_host"
    
    JIRA: https://issues.redhat.com/browse/OSPRH-11828

---
+++ internal/containermanager/podman.go
@@ -0,0 +1,125 @@
+package containermanager
+
+import (
+	"context"
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/containers/podman/v5/pkg/bindings"
+	"github.com/containers/podman/v5/pkg/bindings/containers"
+	"github.com/containers/podman/v5/pkg/bindings/images"
+	"github.com/containers/podman/v5/pkg/specgen"
+	"github.com/openstack-tooling/pulpod/internal/config"
+	"github.com/openstack-tooling/pulpod/logging"
+)
+
+// PodmanManager is a concrete implementation of ContainerManager for Podman.
+type PodmanManager struct {
+	clientCtx context.Context
+}
+
+// NewPodmanManager creates a new PodmanManager instance.
+func NewPodmanManager(containerManagerConfig *config.ContainerManagerConfig) (ContainerManager, error) {
+	socket := containerManagerConfig.Socket
+
+	//NOTE: Should this be a function? if so, where?
+	if strings.HasPrefix(socket, "unix://") {
+		file := strings.TrimPrefix(socket, "unix://")
+
+		if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
+			return nil, err
+		}
+	}
+
+	clientCtx, err := bindings.NewConnection(context.Background(), socket)
+	if err != nil {
+		logging.Log.Error(err)
+		return nil, err
+	}
+	return PodmanManager{clientCtx: clientCtx}, nil
+}
+
+// TODO: Extract container spec to it's own generator function
+func (p PodmanManager) CreateContainer(containerImage string, containerName string) (string, error) {
+	logging.Log.Infof("Creating Podman container with: %s", containerImage)
+	s := specgen.NewSpecGenerator(containerImage, false)
+	s.Name = containerName
+	s.Labels = map[string]string{
+		"pulpodControlled": "true",
+	}
+	s.Command = []string{"sleep", "infinity"}
+
+	createResponse, err := containers.CreateWithSpec(p.clientCtx, s, nil)
+	if err != nil {
+		logging.Log.Errorf("Failed to create Podman container: %s", s.Name)
+		return "", err
+	}
+	return createResponse.ID, nil
+}
+
+// StartContainer starts a created Podman container.
+func (p PodmanManager) StartContainer(containerID string) error {
+	logging.Log.Infof("Starting Podman container: %s", containerID)
+	if err := containers.Start(p.clientCtx, containerID, nil); err != nil {
+		logging.Log.Errorf("Failed to start Podman container: %s\n", containerID)
+		return err
+	}
+	logging.Log.Infof("Container %s started!", containerID)
+	return nil
+}
+
+// StopContainer stops a Podman container.
+func (p PodmanManager) StopContainer(containerID string) error {
+	logging.Log.Infof("Stopping Podman container: %s\n", containerID)
+	if err := containers.Stop(p.clientCtx, containerID, nil); err != nil {
+		logging.Log.Errorf("Failed to stop Podman container: %s\n", containerID)
+		return err
+	}
+	logging.Log.Infof("Container %s stopped!", containerID)
+	return nil
+}
+
+func (p PodmanManager) RemoveContainer(containerID string) error {
+	logging.Log.Infof("Removing %s container\n", containerID)
+	force := true
+	depend := true
+	_, err := containers.Remove(p.clientCtx, containerID, &containers.RemoveOptions{Force: &force, Depend: &depend})
+	if err != nil {
+		logging.Log.Errorf("Failed to remove Podman container: %s\n", containerID)
+		return err
+	}
+	return nil
+}
+
+func (p PodmanManager) ReturnContext() context.Context {
+	ctx := p.clientCtx
+	return ctx
+}
+
+func (p PodmanManager) PullImage(imageName string) error {
+	pullOptions := new(images.PullOptions).WithQuiet(true)
+	_, err := images.Pull(p.clientCtx, imageName, pullOptions)
+	if err != nil {
+		logging.Log.Errorf("Failed to pull Podman image: %s\n", imageName)
+		return err
+	}
+	return nil
+}
+
+func (p PodmanManager) List() ([]string, error) {
+	listOptions := new(containers.ListOptions).WithFilters(map[string][]string{
+		"label": {"pulpodControlled=true"},
+	})
+	retunedContainers, err := containers.List(p.clientCtx, listOptions)
+	if err != nil {
+		logging.Log.Errorln("Failed to list Podman containers")
+		return nil, err
+	}
+
+	containerList := []string{}
+	for _, container := range retunedContainers {
+		containerList = append(containerList, container.Names[0])
+	}
+	return containerList, nil
+}
