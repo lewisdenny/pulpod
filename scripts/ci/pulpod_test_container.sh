ab79bc0b7cfb9c71273fc35f7958ebaea9d8ebc6
Lewis Denny <lewisdenny@me.com>
2024-12-19 00:46:14 +1000

    Lay initial foundation and Podman containermanager implementation
    This patch adds the first main component to the Pulpod project, the
    containermanager interface which will be used by the project to spawn
    container based plugins.
    
    As this is the first main component added to the project some extras are
    also being bundled in the simplest implementation possible, these
    include:
    - Basic logging using `go.uber.org/zap`
    - Podman in Podman testing
    - Config management using `github.com/knadh/koanf/v2`
    
    The meat of the is patch is the containermanager interface, specifically
    the PodmanManager implementation. This is a wrapper around
    `github.com/containers/podman/v5/pkg/bindings` to allow us to spawn
    container based plugins, Pulpod itself will be running in a container
    with the host Podman socket being mounted.
    
    Testing is implemented in a way to best mirror the real topology
    
    Ubuntu provided VM from GitHub
    ------------------------------
    Fedora Podman container "podman_host"
    ------------------------------
    Fedora container "pulpod_test"
    
    This means the code being tested runs inside pulpod_test running on
    Fedora with access to a Podman socket and can spawn containers on
    "pulpod_host"
    
    JIRA: https://issues.redhat.com/browse/OSPRH-11828

---
+++ scripts/ci/pulpod_test_container.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+HOST_CONTAINER_NAME=${HOST_CONTAINER_NAME:-"podman_host"}
+TEST_CONTAINER_NAME=${TEST_CONTAINER_NAME:-"podman_test"}
+TEST_CONTAINER_IMAGE=${TEST_CONTAINER_IMAGE:-"registry.fedoraproject.org/fedora:41"}
+
+run_in_host_container() {
+    local command="$*"
+
+    podman exec "${HOST_CONTAINER_NAME}" bash -c "$command"
+
+    local exit_code=$?
+
+    return $exit_code
+}
+
+run_in_test_container() {
+    local command="$*"
+
+    podman exec "${HOST_CONTAINER_NAME}" bash -c "podman exec \"${TEST_CONTAINER_NAME}\" /bin/bash -c \"$command\""
+    local exit_code=$?
+
+    return $exit_code
+}
+
+create_test_container() {
+    run_in_host_container "podman run -d \
+        --security-opt label:type:container_runtime_t \
+        -v /run/podman/podman.sock:/run/podman/podman.sock:Z \
+        -v /pulpod:/pulpod \
+        --name \"${TEST_CONTAINER_NAME}\" \
+        \"${TEST_CONTAINER_IMAGE}\" \
+        sleep infinity"
+}
+
+bootstrap_test_container() {
+    run_in_test_container "dnf install -y \
+        wget \
+        tput \
+        golang \
+        gpgme-devel \
+        podman-remote"
+}
+
+check_container_running() {
+    container_inspect=$(run_in_host_container podman container inspect -f '{{.State.Running}}' "$TEST_CONTAINER_NAME" 2>/dev/null)
+    if [[ "$container_inspect" == "true" ]]; then
+        exit 0
+    else
+        echo "Container $TEST_CONTAINER_NAME is not running or does not exist."
+        exit 1
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [run|check]"
+    exit 1
+fi
+
+case $1 in
+run)
+    create_test_container
+    bootstrap_test_container
+    ;;
+check)
+    check_container_running
+    ;;
+*)
+    echo "Invalid argument. Use 'run' or 'check'"
+    exit 1
+    ;;
+esac
